7af845ef191d027f71a2b98fbf0a0b811e6ef3d9
ColoredLemon <y.yangster1998@gmail.com>
2024-07-15 16:09:14 -0500

    set up gh-pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold leading-tight text-gray-900">
-          THIS IS THE ABOUT PAGE Name
+          Yuepeng Yang`s About Page
         </h1>
       </div>
     </header>
